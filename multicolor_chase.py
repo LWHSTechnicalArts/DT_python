bf95ea910f7f5af8e9a7bb7964023e8a66c07a4b
Lizzy Brooks <lizzy.brooks@gmail.com>
2022-03-22 09:48:02 -0700

    Create multicolor_chase.py

---
+++ multicolor_chase.py
@@ -0,0 +1,38 @@
+#by Ethan Dunn
+import time 
+import board 
+import neopixel
+
+pixel_pin = board.D2
+num_pixels = 12
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False)
+
+RED = (255, 0, 0) 
+YELLOW = (255, 150, 0) 
+GREEN = (0, 255, 0) 
+CYAN = (0, 255, 255) 
+BLUE = (0, 0, 255) 
+PURPLE = (180, 0, 255) 
+WHITE = (255, 255, 255) 
+OFF = (0, 0, 0) 
+COLOR = WHITE
+
+while True: 
+    for c in range(0, 5, 1): 
+        if (c == 0): 
+            COLOR = RED 
+        if (c == 1): 
+            COLOR = YELLOW 
+        if (c == 2): 
+            COLOR = GREEN 
+        if (c == 3): 
+            COLOR = CYAN 
+        if (c == 4): 
+            COLOR = BLUE 
+        if (c == 5): 
+            COLOR = PURPLE 
+        for i in range(0, 12, 1): 
+            pixels[i] = COLOR 
+            pixels.show() 
+            time.sleep(0.1)
