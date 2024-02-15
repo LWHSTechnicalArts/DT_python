d35132b81810fb35842ddcb36821d6bdb67368e3
Lizzy Brooks <lizzy.brooks@gmail.com>
2024-02-15 11:01:27 -0800

    Create randomColor.py
    This example uses Python's random package to create a random color within a range.

---
+++ randomColor.py
@@ -0,0 +1,17 @@
+import board
+import neopixel
+import time
+import random
+ 
+pixel_pin = board.D2   #the ring data is connected to this pin
+num_pixels = 16        #number of leds pixels on the ring
+ 
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+ 
+ 
+while True:
+    pixels[0] = (random.randint(150,250),random.randint(50,150),0)
+    pixels.show()     #required to update pixels
+    time.sleep(.5)
+    
+  
