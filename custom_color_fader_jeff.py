bb9b54576758685ee02a5f02b72ce241806f6c0e
Jeff Trevino <jeffrey.trevino@gmail.com>
2025-05-15 13:46:01 -0700

    created teachable version ofcusotm color fader and tweaked first version

--- custom_color_fader_jeff.py
+++ custom_color_fader_jeff.py
@@ -17,38 +17,44 @@ YELLOW = (255, 150, 0)
 PURPLE = (180, 0, 255)
 RED = (255, 0, 0)
 
-# helper functions for fading
-# first, to understand this code...
+# helper functions for fading - advanced version
+# start with the simpler version linked above this one — 
+# it is less elegant but easier to read and understand —
+# and then continue to this solution,
+# which is more like what a programmer would actually do in Python
+#
+# to understand this version...
+    # ...study functions
     # ...study zip —
     # https://docs.python.org/3.3/library/functions.html#zip
     # ...study list comprehensions —
     # https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
 
 def crossfade(from_color, to_color, step_wait_time):
-    # setup - How much do we change r, g, and b with each step?
-    rgb_diffs = [to_value - from_value for to_value, from_value in zip(to_color, from_color)]
-    abs_diffs = [abs(diff) for diff in rgb_diffs]
-    max_diff = max(abs_diffs)
-    rgb_increments = [int(diff/abs(diff)) for diff in rgb_diffs] # choose 1 or -1 for r g and b
-
-    # perform the fade
+    print('-' * 10, 'FADING FROM', from_color, 'TO', to_color, '...', '-' * 10)
+    time.sleep(1)
+    # more advanced programmers use list comprehensions
+    # to work with entire lists at a time in each line
+    rgb_changes = [to_value - from_value for to_value, from_value in zip(to_color, from_color)]
+    abs_changes = [abs(change) for change in rgb_changes]
+    max_abs_change = max(abs_changes)
+    rgb_increments = [int(change / abs(change)) for change in rgb_changes]
     now_color = from_color
-    for i in range(0, max_diff):
-        now_color = increment_color(now_color, to_color, rgb_increments)
-        fill_pixels(now_color, num_pixels)
+
+    for i in range(0, max_abs_change):
+        # increment the color
+        incremented_color = []
+        for now_color_value, to_color_value, increment in zip(now_color, to_color, rgb_increments):
+            incremented_value = now_color_value if now_color_value == to_color_value else now_color_value + increment
+            incremented_color.append(incremented_value)
+        now_color = incremented_color
+        # display the color
+        print('now_color:', now_color)
+        for i in range(len(pixels)):
+            pixels[i] = tuple(now_color)
         pixels.show()
+        # wait for the specified step wait time 
         time.sleep(step_wait_time)
-
-def fill_pixels(color, num_pixels):
-    for i in range(len(pixels)):
-        pixels[i] = tuple(color)
-
-def increment_color(now_color, to_color, rgb_increments):
-    incremented_color = []
-    for now_color_value, to_color_value, increment in zip(now_color, to_color, rgb_increments):
-        incremented_value = now_color_value if now_color_value == to_color_value else now_color_value + increment
-        incremented_color.append(incremented_value)
-    return incremented_color
 
 while True:
     crossfade(PURPLE, YELLOW, 0)
---
+++ custom_color_fader_jeff_teachable.py
@@ -0,0 +1,73 @@
+# Custom Color Fader
+
+# Universal color fader helper function
+# Start with this version to understand the process,
+# and then have a look at the more Pythonic solution linked below this one,
+# to see how an experienced programmer would solve this problem in Python.
+# This will fade between two colors at an even rate.
+
+import time
+import board
+import neopixel
+from digitalio import DigitalInOut, Direction, Pull
+
+pixel_pin = board.D2
+num_pixels = 16
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+
+YELLOW = (255, 150, 0)
+PURPLE = (180, 0, 255)
+RED = (255, 0, 0)
+
+
+def crossfade(from_color, to_color, step_wait_time):
+    print('-' * 10, 'FADING FROM', from_color, 'TO', to_color, '...', '-' * 10)
+    time.sleep(1)
+    # How can we fade between any two given colors, no matter their red, green, and blue values?
+    # We need to answer a few questions about our two colors first.
+
+    # Because we want to change red, green, and blue separately,
+    # let's pull out red, green, and blue separately from our from_color and our to_color.
+    from_r, from_g, from_b = from_color
+    to_r, to_g, to_b = to_color
+
+    # How much do red, green, and blue increase or decrease as we move from from_color to to_color?
+    change_r = to_r - from_r
+    change_g = to_g - from_g
+    change_b = to_b - from_b
+
+    # Which color changes the most on the way from from_color to to_color, no matter whether decreasing or increasing in value?
+    # We use the abs() function to get the absolute value of our changes, and we choose the largest value.
+    max_abs_change = max([abs(change_r), abs(change_g), abs(change_b)])
+
+    # Should red, green, and blue increase or decrease on the way from from_color to to_color?
+    # We compare the absolute value of each change against the true value to arrive at a 1 or a -1 for each color.
+    # An increment of -1 will decrease the color's value, while an increment of 1 will increase the color's value.
+    increment_r = int(change_r / abs(change_r))
+    increment_g = int(change_g / abs(change_g))
+    increment_b = int(change_b / abs(change_b))
+
+    # We'll start our fade at from_color,
+    # and keep track of the in-between colors with now_color.
+    # This means we want to start with now_color equal to from_color.
+    now_color = from_color
+    # perform the fade
+    for i in range(0, max_abs_change): # The number of steps in our change is the number of values in the largest change (see line 41).
+        now_r, now_g, now_b = now_color
+        # If we have not arrived at the to_color value, increment the value.
+        new_r = now_r if to_r == now_r else now_r + increment_r
+        new_g = now_g if to_g == now_g else now_g + increment_g
+        new_b = now_b if to_b == now_b else now_b + increment_b
+        
+        now_color = (round(new_r), round(new_g), round(new_b)) # We overwrite now_color with our new color,
+        # and then we light up and log the new color
+        print('now_color:', now_color)
+        for i in range(len(pixels)):
+            pixels[i] = tuple(now_color)
+        pixels.show()
+        time.sleep(step_wait_time) # Wait this long before the next step in our change.
+
+while True:
+    crossfade(PURPLE, YELLOW, 0)
+    crossfade(YELLOW, PURPLE, 0)
