2384a7df260442983d644d74f296d99f82d3bd28
Lizzy Brooks <lizzy.brooks@gmail.com>
2026-02-12 10:46:45 -0800

    Create basicRandom.py

---
+++ basicRandom.py
@@ -0,0 +1,12 @@
+import board
+import neopixel
+import random
+
+pixel_pin = board.D2
+num_pixels = 16
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.7, auto_write=False)
+
+while True:
+    pixels[0] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    pixels.show()
